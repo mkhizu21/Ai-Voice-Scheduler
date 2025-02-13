6074b8f19a625e66affd028f5e4774a68dd79fac
mkhizu21 <muhammad.khizar467@gmail.com>
2025-02-14 00:44:46 +0500

    added files

--- whisp.py
+++ whisp.py
@@ -13,7 +13,19 @@ import json
 load_dotenv()
 
 # ✅ Get values from .env
-SERVICE_ACCOUNT_FILE = json.loads(st.secrets["google"]["service_account_json"])
+SERVICE_ACCOUNT_FILE = {
+    "type": st.secrets["google"]["type"],
+    "project_id": st.secrets["google"]["project_id"],
+    "private_key_id": st.secrets["google"]["private_key_id"],
+    "private_key": st.secrets["google"]["private_key"],
+    "client_email": st.secrets["google"]["client_email"],
+    "client_id": st.secrets["google"]["client_id"],
+    "auth_uri": st.secrets["google"]["auth_uri"],
+    "token_uri": st.secrets["google"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["google"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["google"]["client_x509_cert_url"],
+}
+
 CALENDAR_ID = st.secrets["google"]["CALENDAR_ID"]
 
 # Load Whisper Model
